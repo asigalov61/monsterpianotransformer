063576a837dd76c501a14b9293c7586562c0f6a2
Alex <alexlev61@proton.me>
2025-01-20 02:29:30 -0800

    Add files via upload

--- monsterpianotransformer/monsterpianotransformer.py
+++ monsterpianotransformer/monsterpianotransformer.py
@@ -19,8 +19,8 @@ import random
 
 #===================================================================================================
 
-def generate(model, 
-             input_tokens, 
+def generate(model,
+             input_tokens,
              num_gen_tokens=600,
              num_batches=1,
              temperature=0.9,
@@ -35,8 +35,19 @@ def generate(model,
     device = next(model.parameters()).device.type
    
     with torch.amp.autocast(device_type=device, dtype=torch.bfloat16):
+        
+        num_gen_tokens = max(1, min(2047, num_gen_tokens))
 
-        x = torch.LongTensor([input_tokens] * num_batches).to(device)
+        if len(input_tokens) <= (2048 - num_gen_tokens):
+            
+            inputs = input_tokens
+            prime = []
+            
+        else:
+            inputs = input_tokens[-(2048 - num_gen_tokens):]
+            prime = input_tokens          
+            
+        x = torch.LongTensor([inputs] * num_batches).to(device)
         
         if 0.0 < top_p_value < 1.0:
 
@@ -45,7 +56,7 @@ def generate(model,
                                  temperature=temperature,
                                  filter_logits_fn=top_p,
                                  filter_kwargs={'thres': top_p_value},
-                                 return_prime=return_prime,
+                                 return_prime=False,
                                  verbose=verbose
                                 )
             
@@ -54,19 +65,112 @@ def generate(model,
             out = model.generate(x,
                                  num_gen_tokens,
                                  temperature=temperature,
-                                 return_prime=return_prime,
+                                 return_prime=False,
                                  verbose=verbose
                                 )
             
     y = out.tolist()
+
+    outputs = []
+
+    if return_prime:
+        for o in y:
+            outputs.append(prime + o)
+
+    else:
+        outputs = y
 
     if verbose:
         print('=' * 70)
         print('Done!')
         print('=' * 70)
 
-    return y
+    return outputs
+
+#===================================================================================================
+
+def generate_long(model,
+                  input_tokens,
+                  num_gen_tokens=600,
+                  num_gen_cycles=5,
+                  num_batches=1,
+                  temperature=0.9,
+                  top_p_value=0.0,
+                  return_prime=False,
+                  verbose=False
+                 ):
         
+    if verbose:
+        print('=' * 70)
+        print('Starting generation...')
+
+    device = next(model.parameters()).device.type
+   
+    with torch.amp.autocast(device_type=device, dtype=torch.bfloat16):
+        
+        num_gen_tokens = max(1, min(2047, num_gen_tokens))
+        num_mem_tokens = 2048-num_gen_tokens
+
+        prime = input_tokens
+
+        if len(input_tokens) <= num_mem_tokens:
+            inputs = input_tokens
+            
+        else:
+            inputs = input_tokens[-num_mem_tokens:]
+
+        outputs = [[]] * num_batches
+            
+        for i in range(num_gen_cycles):
+            
+            if verbose:
+                print('=' * 70)
+                print('Generation cycle #', i)
+                print('=' * 70)
+            
+            if i == 0:
+                x = torch.LongTensor([inputs] * num_batches).to(device)
+
+            else:
+                x = torch.LongTensor([o[-num_mem_tokens:] for o in outputs]).to(device)
+            
+            if 0.0 < top_p_value < 1.0:
+    
+                out = model.generate(x,
+                                     num_gen_tokens,
+                                     temperature=temperature,
+                                     filter_logits_fn=top_p,
+                                     filter_kwargs={'thres': top_p_value},
+                                     return_prime=False,
+                                     verbose=verbose
+                                    )
+                
+            else:
+                
+                out = model.generate(x,
+                                     num_gen_tokens,
+                                     temperature=temperature,
+                                     return_prime=False,
+                                     verbose=verbose
+                                    )
+                
+            y = out.tolist()
+        
+            if i == 0 and return_prime:
+                for j, o in enumerate(y):
+                    outputs[j].extend(prime + o)
+        
+            else:
+                for j, o in enumerate(y):
+                    outputs[j].extend(o)
+
+    if verbose:
+        print('=' * 70)
+        print('Done!')
+        print('=' * 70)
+
+    return outputs
+
 #===================================================================================================
 # This is the end of model_loader Python module
 #===================================================================================================
