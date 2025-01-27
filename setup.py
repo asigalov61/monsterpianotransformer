fd1268475d1eb885bfecd8d0b4c907e3e48b847a
Alex <alexlev61@proton.me>
2025-01-26 23:56:20 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="monsterpianotransformer",
-    version="25.1.68",
+    version="25.1.83",
     description="Ultra-fast and very well fitted solo Piano music transformer",
     long_description=long_description,
     long_description_content_type="text/markdown",
